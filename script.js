ed73d17b944aae805c6715c1f57c47247a220820
Ivan Razbegaev <sinnless13@gmail.com>
2021-10-20 22:29:35 +0300

    lesson 15 hard partly done

--- script.js
+++ script.js
@@ -1,34 +1,56 @@
-const DomElement = function (selector, height, width, bg, fontSize) {
+const DomElement = function (selector, height, width, bg) {
     this.selector = selector;
     this.height = height;
     this.width = width;
     this.bg = bg;
-    this.fontSize = fontSize;
+    this.squarePosition = '';
 
     let element;
 
     this.createElement = function () {
         if(this.selector.startsWith(".")){
             element = document.createElement('div');
-            element.classList = this.selector;
+            element.classList = this.selector.split("").map((value, index) => {
+                return index === 0 ? '' : value;
+            }).join("");
             element.style.height = `${this.height}px`;
             element.style.width = `${this.width}px`;
             element.style.backgroundColor = this.bg;
-            element.style.fontSize = `${this.fontSize}px`;
-            element.textContent = "Lorem ipsum dolor sit amet";
+            element.style.position = this.squarePosition;
             document.body.append(element);
         } else if(this.selector.startsWith("#")) {
             element = document.createElement('p');
-            element.id = selector;
+            console.log();
+            element.id = this.selector.split("").map((value, index) => {
+                return index === 0 ? '' : value;
+            }).join("");
             element.style.height = `${this.height}px`;
             element.style.width = `${this.width}px`;
             element.style.backgroundColor = this.bg;
-            element.style.fontSize = `${this.fontSize}px`;
-            element.textContent = "Lorem ipsum dolor sit amet";
+            element.style.position = this.squarePosition;
             document.body.append(element);
         }
     };
 }
 
-const domElement = new DomElement("#block", 25, 25, "green", 12);
-domElement.createElement();
+const domElement = new DomElement("#block", 100, 100, "black");
+
+domElement.setPosition = function (position) {
+    domElement.squarePosition = position;
+    return this;
+};
+
+domElement.setPosition('absolute').createElement();
+const square = document.querySelector('p');
+
+document.addEventListener("keydown", function (event) {
+    if(event.keyCode === 38   ){
+        console.log("arrowUp is pressed!")
+        square.remove();
+        domElement.height -= 10;
+        domElement.createElement();
+    }
+})
+
+
+console.log(square)

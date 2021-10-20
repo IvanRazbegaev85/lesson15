a2726d7c2a52f786049b0eb0d477142df8a7f556
Ivan Razbegaev <sinnless13@gmail.com>
2021-10-20 20:48:59 +0300

    lesson15 done

---
+++ script.js
@@ -0,0 +1,34 @@
+const DomElement = function (selector, height, width, bg, fontSize) {
+    this.selector = selector;
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
+
+    let element;
+
+    this.createElement = function () {
+        if(this.selector.startsWith(".")){
+            element = document.createElement('div');
+            element.classList = this.selector;
+            element.style.height = `${this.height}px`;
+            element.style.width = `${this.width}px`;
+            element.style.backgroundColor = this.bg;
+            element.style.fontSize = `${this.fontSize}px`;
+            element.textContent = "Lorem ipsum dolor sit amet";
+            document.body.append(element);
+        } else if(this.selector.startsWith("#")) {
+            element = document.createElement('p');
+            element.id = selector;
+            element.style.height = `${this.height}px`;
+            element.style.width = `${this.width}px`;
+            element.style.backgroundColor = this.bg;
+            element.style.fontSize = `${this.fontSize}px`;
+            element.textContent = "Lorem ipsum dolor sit amet";
+            document.body.append(element);
+        }
+    };
+}
+
+const domElement = new DomElement("#block", 25, 25, "green", 12);
+domElement.createElement();
